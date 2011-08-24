a97148d3b5fdd306d4121157fe63eecc4b102231
Raffael Schmid <raffi.schmid@gmail.com>
2011-08-24 15:13:34 +0200

    Dokumentation - Analyse der Aufgabenstellung

--- Software/sampling/data/loganalysis-samples/src/main/java/GcTestProgram.java
+++ Software/sampling/data/loganalysis-samples/src/main/java/GcTestProgram.java
@@ -3,14 +3,14 @@ import java.util.Random;
 public class GcTestProgram {
 	private static final int NB_VALUES = 20000000;
 	private static final int MAX_VALUE = 1000;
-	private static final int NB_RUNS = 1000;//20
+	private static final int NB_RUNS = 20;
 
 	public static void main(String[] args) {
-		Integer[] boxedValues = new Integer[NB_VALUES];
 		int[] values = initValues();
 
 		System.out.println("Benchmarking...");
 		for (int run = 1; run <= NB_RUNS; run++) {
+			Integer[] boxedValues = new Integer[NB_VALUES];
 			long t1 = System.currentTimeMillis();
 			for (int i = 0; i < NB_VALUES; i++) {
 				boxedValues[i] = values[i];
