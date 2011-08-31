84fc42be633b5e95aece691f004b613a965c425e
Raffael Schmid <raffi.schmid@gmail.com>
2011-08-31 09:48:01 +0200

    Dokumentation - Mind Map Konzept

--- Dokumentation/Bachelorthesis/model/konzept.mm
+++ Dokumentation/Bachelorthesis/model/konzept.mm
@@ -1,6 +1,11 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1314772786356" ID="ID_1668590878" MODIFIED="1314772817115" TEXT="Konzept">
-<node CREATED="1314772826151" ID="ID_800772938" MODIFIED="1314772830890" POSITION="right" TEXT="Architektur"/>
+<node CREATED="1314772786356" ID="ID_1668590878" MODIFIED="1314773012716" TEXT="Konzept">
+<node CREATED="1314772826151" ID="ID_800772938" MODIFIED="1314776762750" POSITION="right" TEXT="Architektur">
+<node CREATED="1314776762750" ID="ID_1010688883" MODIFIED="1314776774666" TEXT="Plugin / Feature Struktur">
+<node CREATED="1314776696749" ID="ID_1466155405" MODIFIED="1314776703868" TEXT="Aufbau Update-Site"/>
+<node CREATED="1314776723477" ID="ID_758777438" MODIFIED="1314776737345" TEXT="Resultierende Feature"/>
+</node>
+</node>
 </node>
 </map>
