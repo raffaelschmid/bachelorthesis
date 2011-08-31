97dab1d7e50c16dcef519ec5246c98752bed526f
Raffael Schmid <raffi.schmid@gmail.com>
2011-08-31 09:30:37 +0200

    changed version from 1.0.0 to 1.0.0.qualifier / 1.0.0-SNAPSHOT

---
+++ Dokumentation/Bachelorthesis/model/konzept.mm
@@ -0,0 +1,6 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1314772786356" ID="ID_1668590878" MODIFIED="1314772817115" TEXT="Konzept">
+<node CREATED="1314772826151" ID="ID_800772938" MODIFIED="1314772830890" POSITION="right" TEXT="Architektur"/>
+</node>
+</map>
