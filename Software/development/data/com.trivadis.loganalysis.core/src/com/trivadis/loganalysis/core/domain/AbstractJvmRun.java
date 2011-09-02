22eed4cc203f77177671b7c6000bd5e9550c6854
Raffael Schmid <raffi.schmid@gmail.com>
2011-09-03 00:44:49 +0200

    remove unnecessary files

--- Software/development/data/com.trivadis.loganalysis.core/src/com/trivadis/loganalysis/core/domain/AbstractJvmRun.java
+++ Software/development/data/com.trivadis.loganalysis.core/src/com/trivadis/loganalysis/core/domain/AbstractJvmRun.java
@@ -1,9 +1,9 @@
 package com.trivadis.loganalysis.core.domain;
 
-public abstract class AbstractLogFile implements IJvmRun {
+public abstract class AbstractJvmRun implements IJvmRun {
 	private final IFileDescriptor logFileDescriptor;
 	
-	public AbstractLogFile(final IFileDescriptor logFileDescriptor) {
+	public AbstractJvmRun(final IFileDescriptor logFileDescriptor) {
 		this.logFileDescriptor = logFileDescriptor;
 	}
 
--- Software/development/data/com.trivadis.loganalysis.jrockit/src/com/trivadis/loganalysis/jrockit/domain/JRockitJvmRun.java
+++ Software/development/data/com.trivadis.loganalysis.jrockit/src/com/trivadis/loganalysis/jrockit/domain/JRockitJvmRun.java
@@ -3,31 +3,26 @@ package com.trivadis.loganalysis.jrockit.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.trivadis.loganalysis.core.domain.AbstractJvmRun;
 import com.trivadis.loganalysis.core.domain.IFileDescriptor;
-import com.trivadis.loganalysis.core.domain.IJvmRun;
 import com.trivadis.loganalysis.jrockit.domain.gc.GarbageCollection;
 import com.trivadis.loganalysis.jrockit.domain.space.Heap;
 import com.trivadis.loganalysis.jrockit.domain.space.KeepArea;
 import com.trivadis.loganalysis.jrockit.domain.space.OldSpace;
 import com.trivadis.loganalysis.jrockit.domain.space.YoungSpace;
 
-public class JRockitJvmRun implements IJvmRun {
+public class JRockitJvmRun extends AbstractJvmRun {
 
 	private final Heap heap;
-	private final IFileDescriptor descriptor;
 	private final List<GarbageCollection> garbageCollections = new ArrayList<GarbageCollection>();
 
 	public JRockitJvmRun(IFileDescriptor descriptor) {
-		this.descriptor = descriptor;
+		super(descriptor);
 		heap = new Heap(this, new KeepArea(this), new OldSpace(this), new YoungSpace(this));
 	}
 
 	public Heap getHeap() {
 		return heap;
-	}
-
-	public IFileDescriptor getDescriptor() {
-		return descriptor;
 	}
 
 	public void addTransition(GarbageCollection transition) {
